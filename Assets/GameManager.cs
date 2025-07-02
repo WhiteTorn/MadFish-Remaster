a7a16e0ed1d478bc235f92a47427cf3c1bb7b360
White Torn <FaustusTorn@protonmail.com>
2025-07-02 21:12:37 +0400

    Final State

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -7,20 +8,29 @@ public class GameManager : MonoBehaviour
 
     public GameObject foodPrefab;
     public GameObject enemyFishPrefab;
+    public GameObject sharkPrefab;
+
     public int enemyNumber = 5;
     public int foodNumber = 29;
 
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI storyText; // NEW: Story text UI
     public GameObject gameOverUI;
+    public GameObject gameWonUI; // NEW: A UI panel for winning
 
     private int score = 0;
     public int scoreToEatEnemies = 300;
+    public int scoreToDefeatShark = 2000;
 
     void Awake()
     {
         if (instance == null)
         {
             instance = this;
+        }
+        else 
+        {
+            Destroy(gameObject);
         }
     }
 
@@ -29,12 +39,15 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         gameOverUI.SetActive(false);
+        gameWonUI.SetActive(false);
 
         Time.timeScale = 1f;
 
         SpawnAllFood();
         SpawnAllEnemies();
+        SpawnShark();
         UpdateScoreUI();
+        UpdateStory();
     }
 
     public void AddScore(int amount)
@@ -103,6 +116,69 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (gameOverUI.activeSelf || gameWonUI.activeSelf)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                RestartGame();
+            }
+        }
+    }
+
+    public void PlayerHitShark()
+    {
+        if (score >= scoreToDefeatShark)
+        {
+            GameWon();
+        }
+        else
+        {
+            GameOver();
+        }
+    }
+
+    void GameWon()
+    {
+        gameWonUI.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    void UpdateStory()
+    {
+        if (score <= 60)
+        {
+            storyText.text = "A long time ago, there was peace and bliss in the Kingdom of Ungu...";
+        }
+        else if (score <= 200)
+        {
+            storyText.text = "It all started when Ungu's heir was defeated by an evil shark called Zako...";
+        }
+        else if (score <= 500)
+        {
+            storyText.text = "According to legend, the only one who can save the Kingdom will be born...";
+        }
+        else if (score <= 1000)
+        {
+            storyText.text = "In 900 AD, the heir of Ungu was born: Dex of Ungu.";
+        }
+        else if (score <= 2000)
+        {
+            storyText.text = "Dex was small, but had the spirit of a true warrior...";
+        }
+        else
+        {
+            storyText.text = "Dex grew stronger. The time has come to face Zako!";
+        }
+    }
+
+    void SpawnShark()
+    {
+        // Spawn the shark at position (0, 0, 0)
+        Instantiate(sharkPrefab, Vector3.zero, Quaternion.identity);
     }
 }
