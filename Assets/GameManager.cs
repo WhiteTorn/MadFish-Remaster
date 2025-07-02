a839b78099cda7734fa4e164d92b150bc1d52a99
White Torn <FaustusTorn@protonmail.com>
2025-07-02 19:44:55 +0400

    Setting Up GameManager

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -2,10 +2,31 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    
+    public GameObject foodPrefab;
+    public int foodNumber = 29;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        SpawnAllFood();
+    }
+
+    void SpawnAllFood()
+    {
+        Camera cam = Camera.main;
+        float height = 2f * cam.orthograpicSize;
+        float widht = height * cam.aspect;
+
+        for (int i = 0; i < foodNumber; i++)
+        {
+            float randomX = Random.Range(-width / 2, width / 2);
+            float randomY = Random.Range(-height / 2, height / 2);
+            Vector2 spawnPosition = new Vector2(randomX, randomY);
+
+            Instantiate(foodPrefab, spawnPosition, Quaternion.identity);
+        }
     }
 
     // Update is called once per frame
