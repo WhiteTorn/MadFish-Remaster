cbc3bd7ed6bec1a34f5b14f79dbb35a26c7aed74
White Torn <FaustusTorn@protonmail.com>
2025-07-02 20:45:31 +0400

    Game Logic Update to add Enemy Fishes

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -6,11 +6,15 @@ public class GameManager : MonoBehaviour
     public static GameManager instance;
 
     public GameObject foodPrefab;
+    public GameObject enemyFishPrefab;
+    public int enemyNumber = 5;
     public int foodNumber = 29;
 
     public TextMeshProUGUI scoreText;
+    public GameObject gameOverUI;
 
     private int score = 0;
+    public int scoreToEatEnemies = 300;
 
     void Awake()
     {
@@ -24,7 +28,12 @@ public class GameManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        gameOverUI.SetActive(false);
+
+        Time.timeScale = 1f;
+
         SpawnAllFood();
+        SpawnAllEnemies();
         UpdateScoreUI();
     }
 
@@ -32,6 +41,26 @@ public class GameManager : MonoBehaviour
     {
         score += amount;
         UpdateScoreUI();
+    }
+
+    public void PlayerHitEnemy(GameObject enemy)
+    {
+        if (score >= scoreToEatEnemies)
+        {
+            AddScore(350);
+            Destroy(enemy);
+        }
+        else
+        {
+            GameOver();
+        }
+    }
+
+    void GameOver()
+    {
+        Debug.log("Game Over!");
+        gameOverUI.SetActive(true);
+        Time.timeScale = 0f;
     }
 
     void UpdateScoreUI()
@@ -52,6 +81,22 @@ public class GameManager : MonoBehaviour
             Vector2 spawnPosition = new Vector2(randomX, randomY);
 
             Instantiate(foodPrefab, spawnPosition, Quaternion.identity);
+        }
+    }
+
+    void SpawnAllEnemies()
+    {
+        Camera cam = Camera.main;
+        float height = 2f * cam.orthographicSize;
+        float width = height * cam.aspect;
+
+        for (int i = 0; i < enemyNumber; i++)
+        {
+            float randomX = Random.Range(-width / 2, width / 2);
+            float randomY = Random.Range(-height / 2, height / 2);
+            Vector2 spawnPosition = new Vector2(randomX, randomY);
+
+            Instantiate(enemyFishPrefab, spawnPosition, Quaternion.identity);
         }
     }
 
