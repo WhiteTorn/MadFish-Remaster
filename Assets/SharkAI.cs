f668e67511331f7f826c40202f9e53b2e028bea1
White Torn <FaustusTorn@protonmail.com>
2025-07-03 12:42:45 +0400

    Shark AI first version

--- Assets/SharkAI.cs
+++ Assets/SharkAI.cs
@@ -2,15 +2,42 @@ using UnityEngine;
 
 public class SharkAI : MonoBehaviour
 {
+    public float moveSpeed = 1f;
+
+    private Transform playerTransform;
+    private SpriteRenderer spriteRenderer;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null)
+        {
+            playerTransform = player.transform;
+        }
         
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (playerTransform == null)
+        {
+            return;
+        }
+
+        Vector2 direction = (playerTransform.position - transform.position).normalized;
+    
+        transform.position += (Vector3)direction * moveSpeed * Time.deltaTime;
+    
+        if (direction.x > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if(direction.x < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
     }
 }
